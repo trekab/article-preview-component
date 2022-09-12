22133e4ebcf745009a27dcf8d5427727e32589fc
Treasure Kabareebe <treasurecalm@gmail.com>
2022-09-12 20:01:16 +0300

    feat: Add the active states for small devices

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+const share = document.getElementById("share-1");
+const hideShare = document.getElementById("share-2");
+const authorDetails = document.getElementsByClassName("author")[0];
+const shareOptions = document.getElementsByClassName("share-wrapper")[0];
+
+const displayShareOptions = () => {
+  authorDetails.classList.add("d-none");
+  shareOptions.classList.remove("d-none");
+};
+
+const hideShareOptions = () => {
+  authorDetails.classList.remove("d-none");
+  shareOptions.classList.add("d-none");
+};
+
+share.addEventListener("click", displayShareOptions);
+hideShare.addEventListener("click", hideShareOptions);
